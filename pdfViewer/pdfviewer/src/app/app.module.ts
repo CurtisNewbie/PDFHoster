d15cd9e2a3e61cd1461699fff44a5029d29b095e
CurtisNewbie <zhuangyongj@gmail.com>
2020-05-20 17:14:48 +0800

    Impl HttpService

--- pdfViewer/pdfviewer/src/app/app.module.ts
+++ pdfViewer/pdfviewer/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { PdfJsViewerModule } from 'ng2-pdfjs-viewer';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PdfJsViewerModule
+    PdfJsViewerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ pdfViewer/pdfviewer/src/app/http.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const BASEURL = `http://${location.hostname}:8080`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http: HttpClient) { }
+
+  getFileNames(): Observable<string[]> {
+    return this.http.get<string[]>(`${BASEURL}/file/names`, { responseType: "json" });
+  }
+
+  getFile(filename: string): Observable<any> {
+    let headers: HttpHeaders = new HttpHeaders({ 'filename': filename });
+    return this.http.get(`${BASEURL}/file`, { headers: headers, responseType: 'blob', observe: "body" });
+  }
+}
