162bcb28673449d87ec18fc1d6b38be4ec68117f
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-17 12:07:49 +0800

    Impl IOConfig where all @ConfigProperty are injected

---
+++ pdfHoster/pdfhoster/src/main/java/com/curtisnewbie/config/IOConfig.java
@@ -0,0 +1,28 @@
+package com.curtisnewbie.config;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import io.quarkus.arc.config.ConfigProperties;
+
+@ConfigProperties(prefix = "io.config")
+public interface IOConfig {
+
+    /**
+     * Get teh default filename used for file uploading.
+     * 
+     * @return default filename
+     */
+    @ConfigProperty(name = "def.filename")
+    String defaultFileName();
+
+    /**
+     * The path to the directory which is scanned and watched for changes. This
+     * directory should contain the PDFs files that will be hosted.
+     * 
+     * @return path to directory
+     */
+    @ConfigProperty(name = "scan.dir")
+    String scannedDir();
+}
