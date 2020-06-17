227c709048c07429c510f8cb367a2f0fe6dc734a
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-17 12:16:18 +0800

    Refactor FileManager

--- pdfHoster/pdfhoster/src/main/java/com/curtisnewbie/io/FileManager.java
+++ pdfHoster/pdfhoster/src/main/java/com/curtisnewbie/io/FileManager.java
@@ -5,7 +5,9 @@ import java.util.concurrent.*;
 import java.io.*;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.ObservesAsync;
-import javax.inject.Inject;
+
+import com.curtisnewbie.config.IOConfig;
+
 import org.jboss.logging.Logger;
 
 /**
@@ -15,27 +17,32 @@ import org.jboss.logging.Logger;
  * <p>
  * ------------------------------------
  * <p>
- * Class that provides methods to retrieve files discovered in directory (which might or might not
- * be PDFs).
+ * Class that provides methods to retrieve files discovered in directory (which
+ * might or might not be PDFs).
  * </p>
  */
 @ApplicationScoped
 public class FileManager {
 
     private static final Logger logger = Logger.getLogger(FileManager.class);
+    private final FileScanner scanner;
+    private final String DEFAULT_FNAME;
+    private final String DIR;
+    private final ConcurrentMap<String, File> files = new ConcurrentHashMap<>();
 
-    @Inject
-    protected FileScanner scanner;
-
-    private Map<String, File> files = new ConcurrentHashMap<>();
+    public FileManager(FileScanner scanner, IOConfig ioConfig) {
+        this.scanner = scanner;
+        this.DIR = ioConfig.scannedDir();
+        this.DEFAULT_FNAME = ioConfig.defaultFileName();
+    }
 
     protected void onScannerStart(@ObservesAsync ScannerStartup ss) {
         updateFiles(); // scan files for the first time
     }
 
     /**
-     * Handle {@code DirChangeEvent} fired by {@code FileScanner} and update the internal Map
-     * accordingly.
+     * Handle {@code DirChangeEvent} fired by {@code FileScanner} and update the
+     * internal Map accordingly.
      * 
      * @param ce a DirChangeEvent indicating the change of the directory
      */
@@ -44,27 +51,28 @@ public class FileManager {
     }
 
     /**
-     * This method undertakes two operations, first one is to iterate the Map {@code files} to
-     * remove files that don't exist. The second operation is to scan the directory, and update the
-     * Map {@code files} only for new discovered entries.
+     * This method undertakes two operations, first one is to iterate the Map
+     * {@code files} to remove files that don't exist. The second operation is to
+     * scan the directory, and update the Map {@code files} only for new discovered
+     * entries.
      */
     private void updateFiles() {
+        this.files.forEach((k, v) -> {
+            if (!v.exists())
+                files.remove(k);
+        });
         Map<String, File> scannedFiles = scanner.scanDir();
-        for (Map.Entry<String, File> entry : this.files.entrySet())
-            if (!entry.getValue().exists())
-                files.remove(entry.getKey());
-
         for (Map.Entry<String, File> entry : scannedFiles.entrySet())
-            if (!files.containsKey(entry.getKey()))
-                files.put(entry.getKey(), entry.getValue());
+            files.putIfAbsent(entry.getKey(), entry.getValue());
     }
 
     /**
      * Get the names of all files.
      * <p>
-     * These names can be used later to retrieve the actual file. The returned List is not backed by
-     * any data structure (e.g., Map), thus will not be updated accordingly once returned. I.e., it
-     * might only reflect the view that it's created.
+     * These names can be used later to retrieve the actual file. The returned List
+     * is not backed by any data structure (e.g., Map), thus will not be updated
+     * accordingly once returned. I.e., it might only reflect the view when it's
+     * created.
      * 
      * @return list of names of all files discovered
      */
@@ -96,10 +104,10 @@ public class FileManager {
      * @return File or NULL if IOException is caught
      */
     public File createFile(String name) {
-        logger.info("Create file " + name);
         if (name == null)
-            name = "random.pdf";
-        File file = new File(scanner.getDir(), name);
+            name = DEFAULT_FNAME;
+        logger.info("Create file " + name);
+        File file = new File(DIR, name);
         try {
             file.createNewFile();
         } catch (IOException e) {
